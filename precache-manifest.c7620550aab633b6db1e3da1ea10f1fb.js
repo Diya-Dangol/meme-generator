f6daaee2e6e67c9bce574befbaeb2e53b34a722c
Diya-Dangol <diya.dangol15@gmail.com>
2020-10-07 12:02:20 +0545

    Updates

---
+++ precache-manifest.c7620550aab633b6db1e3da1ea10f1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5181f781fb47428bdad1cb69a9af3241",
+    "url": "/mem-generator/index.html"
+  },
+  {
+    "revision": "a3363e94b8058e3674f0",
+    "url": "/mem-generator/static/css/main.766a4379.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/mem-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mem-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3363e94b8058e3674f0",
+    "url": "/mem-generator/static/js/main.1013a647.chunk.js"
+  },
+  {
+    "revision": "0da44c1b4f3130462e0d",
+    "url": "/mem-generator/static/js/runtime-main.98c86aec.js"
+  }
+]);
